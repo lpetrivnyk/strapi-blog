da8ddc513e85d8d75ab717af32ce5724a1e51e5b
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2023-06-14 16:58:06 +0300

    Fixed Netlify

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    // secret: env('ADMIN_JWT_SECRET'),
+    secret: 't1/viXYBv3wrj2g6owx0rg==',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    // salt: env('API_TOKEN_SALT'),
+    salt: 'SfBBR1nJGUyULo8tGKIqQA==',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: 'SfBBR1nJGUyULo8tGKIqQA==',
     },
   },
 });
