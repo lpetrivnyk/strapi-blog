72be0b9e0896b36ad5fb3dd7163c32093081f2e0
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2023-06-16 14:39:58 +0300

    Fixed deploy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://strapi-blog.netlify.app',
+  url: 'https://strapi-blog-petrivnyk.netlify.app',
   app: {
     keys: env.array('APP_KEYS'),
   },
