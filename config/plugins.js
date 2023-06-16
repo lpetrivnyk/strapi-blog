368fddbc2a0a3ae23f7d581da571e48fc62c7912
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2023-06-16 13:55:46 +0300

    Fixed deploy

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,5 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   "netlify-deployments": {
-//     sites: [
-//       {
-//         title: 'Your site name',
-//         apiId: 'a44185a9-2757-44b3-bf38-071da20cdc60', // Отримайте його з налаштувань "General" вашого сайту на Netlify
-//         buildHookId: '6489bd552d8f1624fd61dc81' // Остання частина URL вашого Build Hook (наприклад, https://api.netlify.com/build_hooks/xxxxxxxxxxxxxxxxxxxxxxxx)
-//       }
-//     ]
-//   },
-// });
+module.exports = ({ env }) => ({
+  "netlify-deployments": {
+    enabled: true,
+  },
+});
