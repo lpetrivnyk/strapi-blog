d446aba4612e2223c0d4aad9955b4a9df6feaf9d
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2023-06-16 14:36:49 +0300

    Fixed deploy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('MY_NETLIFY_URL'),
+  url: 'https://extraordinary-daifuku-6d7adb.netlify.app',
   app: {
     keys: env.array('APP_KEYS'),
   },
